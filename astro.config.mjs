b4a6a65202895dbbb4b7875b76226c8548027bba
JuanRoccia <juanroccia@gmail.com>
2023-07-25 18:19:24 -0300

    mensaje del commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: process.env.NODE_ENV === 'production'
+    ? 'https://juanroccia.github.io/juanroccia.github.io'
+    : 'http://localhost:5000'
+});
