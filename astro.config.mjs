3b8070eea5f1f8fa027321c45f744ec00c2151dd
JuanRoccia <juanroccia@gmail.com>
2023-07-25 19:51:14 -0300

    new deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-    site: process.env.NODE_ENV === 'production'
-    ? 'https://juanroccia.github.io/juanroccia.github.io'
-    : 'http://localhost:5000'
-});
+  site: 'https://juanroccia.github.io'
+})
