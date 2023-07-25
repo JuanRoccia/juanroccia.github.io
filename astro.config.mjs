79ec8e162f17565cbe781482bd67dbe684a32db3
JuanRoccia <juanroccia@gmail.com>
2023-07-25 17:47:00 -0300

    new routs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
-// import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-// // https://astro.build/config
-// export default defineConfig({});
-import { defineConfig } from 'astro';
-
-export default defineConfig({
-  buildOptions: {
-    out: 'docs/', // directorio de salida
-  },
-  publicPath: '/juanroccia.github.io/', // ruta pública
-});
+// https://astro.build/config
+export default defineConfig({});
